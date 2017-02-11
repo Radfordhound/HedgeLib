773ecbf78e53a362a080e8a1257e38775b690215
Radfordhound <radfordhound@gmail.com>
2017-02-10 22:30:31 -0600

    Added some constructors to the ArchiveFile class.

--- HedgeLib/Archives/ArchiveFile.cs
+++ HedgeLib/Archives/ArchiveFile.cs
@@ -8,6 +8,25 @@ namespace HedgeLib.Archives
         public byte[] Data;
         public string Name;
 
+        //Constructors
+        public ArchiveFile() { }
+        public ArchiveFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("The given file could not be found.",
+                    filePath);
+
+            var fileInfo = new FileInfo(filePath);
+            Name = fileInfo.Name;
+            Data = File.ReadAllBytes(filePath);
+        }
+
+        public ArchiveFile(string name, byte[] data)
+        {
+            Name = name;
+            Data = data;
+        }
+
         //Methods
         public void Extract(string filePath)
         {
