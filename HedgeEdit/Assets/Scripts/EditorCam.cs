877980a7360fdcd141d7bdf9386cf0e31e157675
Radfordhound <radfordhound@gmail.com>
2017-03-14 00:39:05 -0500

    Added fast camera movement (hold left shift). Also removed unnecessary cam variable leftover from an older version of this class - whoops!

--- HedgeEdit/Assets/Scripts/EditorCam.cs
+++ HedgeEdit/Assets/Scripts/EditorCam.cs
@@ -1,25 +1,19 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
 public class EditorCam : MonoBehaviour
 {
     //Variables/Constants
-    public const float StartSpeed = 0.01f,
-        SpeedCap = 1f, Accel = 0.05f;
+    public const float StartSpeed = 0.01f, SpeedCap = 1f,
+        FastSpeedCap = 4f, Accel = 0.05f;
 
-    private Camera cam;
     private float pitch = 0, yaw = 0, spd = StartSpeed;
     private bool hasMoved = false;
 
     //Unity Events
-    private void Start()
-    {
-        cam = GetComponent<Camera>();
-    }
-
     private void Update()
     {
         bool isRBDown = Input.GetMouseButton(1);
+        bool fastMove = Input.GetKey(KeyCode.LeftShift);
         hasMoved = false;
 
         Cursor.visible = !isRBDown;
@@ -39,21 +33,21 @@ public class EditorCam : MonoBehaviour
             //Forward/Backwards
             if (Input.GetKey(KeyCode.W))
             {
-                Move(transform.forward, false);
+                Move(transform.forward, false, fastMove);
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                Move(transform.forward, true);
+                Move(transform.forward, true, fastMove);
             }
 
             //Left/Right
             if (Input.GetKey(KeyCode.D))
             {
-                Move(transform.right, false);
+                Move(transform.right, false, fastMove);
             }
             else if (Input.GetKey(KeyCode.A))
             {
-                Move(transform.right, true);
+                Move(transform.right, true, fastMove);
             }
         }
 
@@ -65,12 +59,13 @@ public class EditorCam : MonoBehaviour
 	}
 
     //Methods
-    private void Move(Vector3 direction, bool reverse)
+    private void Move(Vector3 direction, bool reverse, bool fastMove)
     {
         float sign = (reverse) ? -1 : 1;
+        float maxSpd = (fastMove) ? FastSpeedCap : SpeedCap;
 
         transform.position += (direction * spd) * sign;
-        spd = Mathf.Min(spd + Accel, SpeedCap);
+        spd = Mathf.Min(spd + Accel, maxSpd);
         hasMoved = true;
     }
 }
