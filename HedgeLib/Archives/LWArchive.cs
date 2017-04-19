d4d9c9ff132ccdcd7c0535c395c1edd70ae2eaac
Radfordhound <radfordhound@gmail.com>
2017-04-19 02:07:43 -0500

    Added constant Extension string/Constructors to LWArchive

--- HedgeLib/Archives/LWArchive.cs
+++ HedgeLib/Archives/LWArchive.cs
@@ -4,6 +4,16 @@ namespace HedgeLib.Archives
 {
     public class LWArchive : Archive
     {
+		//Variables/Constants
+		public const string Extension = ".pac";
+
+		//Constructors
+		public LWArchive() { }
+		public LWArchive(Archive arc)
+		{
+			Files = arc.Files;
+		}
+
         //Methods
         public override void Load(Stream fileStream)
         {
