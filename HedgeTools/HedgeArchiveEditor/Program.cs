d8a9e2ec585b8f774fa571d9607633cd6eeaefad
thesupersonic16 <supersonic16bit@gmail.com>
2017-04-25 10:20:02 +1000

    HedgeArchiveEditor Removed Console Attaching
    HedgeArchiveEditor Removed Console Attaching because It causes problems
    when launched without a console.

--- HedgeTools/HedgeArchiveEditor/Program.cs
+++ HedgeTools/HedgeArchiveEditor/Program.cs
@@ -21,11 +21,6 @@ namespace HedgeArchiveEditor
                 ShowGUI();
             else
             {
-                AttachConsole(-1);
-                int cursorPosition = Console.CursorTop;
-                Console.SetCursorPosition(0, cursorPosition);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, cursorPosition-1);
                 switch (args[0].ToLower())
                 {
                     case "/e":
@@ -38,14 +33,10 @@ namespace HedgeArchiveEditor
 
                             try
                             {
-                                Console.Write("Loading Archive...");
-                                
                                 //Load the archive
                                 FileInfo fileInfo = new FileInfo(args[1]);
                                 var arc = LoadArchive(args[1]);
 
-                                Console.WriteLine(" Done.");
-                                Console.Write("Extracting...");
                                 //Extract it's contents
                                 var dir = (args.Length < 3) ?
                                     Path.Combine(fileInfo.DirectoryName,
@@ -55,9 +46,7 @@ namespace HedgeArchiveEditor
 
                                 Directory.CreateDirectory(dir);
                                 arc.Extract(dir);
-                                Console.WriteLine(" Done.");
                                 // Prints the working directory.
-                                Console.Write(Directory.GetCurrentDirectory() + ">");
                             }
                             catch (Exception ex)
                             {
@@ -145,9 +134,6 @@ namespace HedgeArchiveEditor
             MainForm = new MainFrm();
             Application.Run(MainForm);
         }
-
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        static extern bool AttachConsole(int dwProcessId);
 
     }
 }
