128faf5ee435f1e03e7b7a919aab1a13cf09ace7
Radfordhound <radfordhound@gmail.com>
2017-04-26 00:28:27 -0500

    Re-arrangement of collision classes

--- HedgeLib/Collison/Havok.cs
+++ HedgeLib/Collison/Havok.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
 
-namespace HedgeLib.Havok
+namespace HedgeLib.Collision
 {
     public class HavokPackFile : FileBase
     {
