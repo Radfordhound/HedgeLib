4760baf6d3422ae0fc8bb6a9f0c0468596ea18bc
Radfordhound <radfordhound@gmail.com>
2017-04-30 22:38:48 -0500

    Fixed compile bug in HedgeEdit

--- HedgeEdit/Assets/Scripts/Convert.cs
+++ HedgeEdit/Assets/Scripts/Convert.cs
@@ -7,7 +7,6 @@ public static class Convert
     {
         //HedgeLib Vectors use X-left, Y-up, Z-forward.
         //Unity Vectors use X-forward, Y-up, Z-left.
-        if (vect == null) return Vector3.zero;
         return new Vector3(vect.Z, vect.Y, vect.X);
     }
 
