2841dbb7d949b0ec8255b5110b75674314f75dc9
thesupersonic16 <supersonic16bit@gmail.com>
2017-05-06 15:41:29 +1000

    Added a horrible WIP Save Method for SBArchive

--- HedgeLib/Archives/SBArchive.cs
+++ HedgeLib/Archives/SBArchive.cs
@@ -68,7 +68,81 @@ namespace HedgeLib.Archives
 			}
 		}
 
-		//TODO: Make Write Method.
+		//TODO: Make a Proper Write/Save Method.
+		public override void Save(Stream fileStream)
+		{
+			// HEADER
+			var writer = new ExtendedBinaryWriter(fileStream, Encoding.ASCII, true);
+			uint dataOffset = (uint)((stringBufferSize + 16) * Files.Count + 16);
+			
+			// Writes the file count.
+			writer.Write((uint)Files.Count);
+
+			// Writes the offset to the file entries.
+			writer.Write((uint)0x10);
+			
+			// Writes the offset to where the data starts.
+			writer.Write(dataOffset);
+			
+			// Unknown, Seems to always be null.
+			writer.Write(0x0);
+
+			// DATA
+			// Writes enough nulls to store the file entries. Probably a bad idea.
+			writer.WriteNulls((uint)((stringBufferSize + 16) * Files.Count));
+			
+			for (int i = 0; i < Files.Count; ++i)
+			{
+				int fileEntryPosition = i * 48 + 16;
+				
+				// Gets the compressed data array.
+				var compressedBytes = CompressPRS(Files[i].Data);
+				
+				// Writes the file name to +0x000000.
+				writer.Seek(fileEntryPosition, SeekOrigin.Begin);
+				writer.Write(Files[i].Name.ToCharArray());
+
+				// Writes file index to +0x000020.
+				writer.Seek(fileEntryPosition + 32, SeekOrigin.Begin);
+				writer.Write((uint)i);
+
+				// Writes the data offset to +0x000024.
+				writer.Write(dataOffset);
+
+				// Adds the compressed size to the offset for the next file.
+				dataOffset += (uint)compressedBytes.Length;
+
+				// Writes the data size to +0x000028.
+				writer.Write((uint)compressedBytes.Length);
+
+				// Writes the uncompressed size to +0x00002C.
+				writer.Write((uint)Files[i].Data.Length);
+			}
+
+			dataOffset = (uint)((stringBufferSize + 16) * Files.Count + 16);
+
+			// Writes all the compressed data.
+			for (int i = 0; i < Files.Count; ++i)
+			{
+				// Seeks to the current file data offset.
+				writer.Seek((int)dataOffset, SeekOrigin.Begin);
+
+				// Gets the compressed data.
+				var compressedBytes = CompressPRS(Files[i].Data);
+
+				// Adds the compressed size to the offset for the next file.
+				dataOffset += (uint)compressedBytes.Length;
+
+				// Writes all the compressed data into the file.
+				writer.Write(compressedBytes);
+			}
+		}
+
+		// TODO: Make a Compression and Decompression Method for both SBArchive and ONEArchive.
+		public static byte[] CompressPRS(byte[] bytes)
+		{
+			return bytes;
+		}
 
 		//Other
 		private struct FileEntry
