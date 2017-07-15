f305d5b95509fd355924a0f72b8103540870afa4
SKmaric <brendan2210@hotmail.com>
2017-07-15 19:36:40 +1000

    Temporary solution for exporting Generations XML file
    Only Position is modified for compatibility. Any speed, distance or
    position related parameters still need to be multiplied by 0.1,
    collision trigger sizes probably by 0.2.

--- HedgeLib/Sets/SetData.cs
+++ HedgeLib/Sets/SetData.cs
@@ -263,5 +263,145 @@ namespace HedgeLib.Sets
 				return new XElement(name, posElem, rotElem, scaleElem);
 			}
 		}
+
+        //Temporary solution for exporting Generations XML file. Delete when proper solution is developed.
+        public void GensExportXML(string filePath,
+            Dictionary<string, SetObjectType> objectTemplates = null)
+        {
+            using (var fileStream = File.OpenWrite(filePath))
+            {
+                GensExportXML(fileStream, objectTemplates);
+            }
+        }
+
+        public void GensExportXML(Stream fileStream,
+            Dictionary<string, SetObjectType> objectTemplates = null)
+        {
+            //Convert to XML file and save
+            var rootElem = new XElement("SetObject");
+
+            foreach (var obj in Objects)
+            {
+                //Generate Object Element
+                //Messy rename to prevent error caused by element name starting with a number.
+                if (obj.ObjectType == "1UP")
+                {
+                    obj.ObjectType = "Item";
+                }
+                var objElem = new XElement(obj.ObjectType);
+
+                //Generate CustomData Element
+                //Messy use RangeOut value as Range value.
+                foreach (var customData in obj.CustomData)
+                {
+                    if (customData.Key == "RangeOut")
+                    {
+                        objElem.Add(GenerateParamElementGens(
+                            customData.Value, "Range"));
+                    }
+
+                }
+
+                //Generate Parameters Element
+                var template = objectTemplates?[obj.ObjectType];
+
+                for (int i = 0; i < obj.Parameters.Count; ++i)
+                {
+                    var name = template?.Parameters[i].Name;
+                    objElem.Add(GenerateParamElementGens(obj.Parameters[i],
+                        template?.Parameters[i].Name));
+                }
+
+                //Generate Transforms Elements
+                objElem.Add(GeneratePositionElement(obj.Transform));
+                objElem.Add(GenerateRotationElement(obj.Transform));
+
+                //Generate ID Element
+                var objIDAttr = new XElement("SetObjectID", obj.ObjectID);
+                objElem.Add(objIDAttr);
+
+                ////Generate MultiSet Elements
+                if (obj.Children.Length > 0)
+                {
+                    var multiElem = new XElement("MultiSetParam");
+
+                    for (int i = 0; i < obj.Children.Length; ++i)
+                    {
+                        var childElem = new XElement("Element");
+                        childElem.Add(new XElement("Index", i + 1));
+                        childElem.Add(GeneratePositionElement(obj.Children[i]));
+                        childElem.Add(GenerateRotationElement(obj.Children[i]));
+                        multiElem.Add(childElem);
+                    }
+                    multiElem.Add(new XElement("BaseLine", 1));
+                    multiElem.Add(new XElement("Direction", 0));
+                    multiElem.Add(new XElement("Interval", 1));
+                    multiElem.Add(new XElement("IntervalBase", 0));
+                    multiElem.Add(new XElement("PositionBase", 0));
+                    multiElem.Add(new XElement("RotationBase", 0));
+                    objElem.Add(multiElem);
+                }
+
+                //Add all of this to the XDocument
+                rootElem.Add(objElem);
+            }
+
+            var xml = new XDocument(rootElem);
+            xml.Save(fileStream);
+
+            //Sub-Methods
+            XElement GenerateParamElementGens(
+                SetObjectParam param, string name)
+            {
+                var dataType = param.DataType;
+                var elem = new XElement((string.IsNullOrEmpty(name)) ?
+                    "Parameter" : name);
+
+                if (dataType == typeof(Vector3))
+                {
+                    Helpers.XMLWriteVector3(elem, (Vector3)param.Data);
+                }
+                else if (dataType == typeof(Vector4) || dataType == typeof(Quaternion))
+                {
+                    Helpers.XMLWriteVector4(elem, (Vector4)param.Data);
+                }
+                else
+                {
+                    elem.Value = param.Data.ToString();
+                }
+
+                return elem;
+            }
+
+            XElement GeneratePositionElement(
+                SetObjectTransform transform, string name = "Transform")
+            {
+                //Convert Position into elements.
+                var posElem = new XElement("Position");
+
+                //Scaling
+                transform.Position.X = (transform.Position.X / 10);
+                transform.Position.Y = (transform.Position.Y / 10);
+                transform.Position.Z = (transform.Position.Z / 10);
+
+                Helpers.XMLWriteVector3(posElem, transform.Position);
+
+                //Add elements to new position element and return it.
+                return new XElement(posElem);
+            }
+
+            XElement GenerateRotationElement(
+                SetObjectTransform transform, string name = "Transform")
+            {
+                //Convert Rotation into elements.
+                var rotElem = new XElement("Rotation");
+
+                Helpers.XMLWriteVector4(rotElem, transform.Rotation);
+
+                //Add elements to new rotation element and return it.
+                return new XElement(rotElem);
+            }
+        }
+        //End of temp solution.
     }
 }
