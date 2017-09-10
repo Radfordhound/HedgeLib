af3141ffc877eb6868844093357a4d77faaa4a50
thesupersonic16 <supersonic16bit@gmail.com>
2017-09-10 21:12:10 +1000

    Added Enum to SetObjectType
    If this works, It should let us create a list of known values inside of
    the Templates.

--- HedgeLib/Sets/SetObjectType.cs
+++ HedgeLib/Sets/SetObjectType.cs
@@ -90,6 +90,23 @@ namespace HedgeLib.Sets
                             Types.GetDefaultFromType(dataType) :
                             Helpers.ChangeType(defaultAttr.Value, dataType)
                     };
+
+                    // Enums
+                    foreach (var enumElement in element.Elements())
+                    {
+                        if (enumElement.Name != "Enum")
+                            continue;
+
+                        var valueAttr = enumElement.Attribute("value");
+                        descAttr = enumElement.Attribute("description");
+
+                        var enumType = new SetObjectTypeParamEnum()
+                        {
+                            Value = valueAttr.Value,
+                            Description = (descAttr == null) ? "" : descAttr.Value
+                        };
+                        param.Enums.Add(enumType);
+                    }
                     Parameters.Add(param);
                 }
             }
@@ -144,6 +161,7 @@ namespace HedgeLib.Sets
         public object DefaultValue;
         public Type DataType;
         public string Name, Description;
+        public List<SetObjectTypeParamEnum> Enums = new List<SetObjectTypeParamEnum>();
 
         // Constructors
         public SetObjectTypeParam() { }
@@ -151,6 +169,19 @@ namespace HedgeLib.Sets
         {
             Name = name;
             DataType = dataType;
+        }
+    }
+
+    public class SetObjectTypeParamEnum
+    {
+        // Variables/Constants
+        public string Description;
+        public object Value;
+
+        // Methods
+        public override string ToString()
+        {
+            return $"{Description} ({Value})";
         }
     }
 }
