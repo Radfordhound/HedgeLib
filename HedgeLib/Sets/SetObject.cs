f0b371b7a6aa4898d8e96801459a869dc347aec2
Radfordhound <grahamlrscott@gmail.com>
2018-04-09 12:39:03 -0500

    Fixed crashing upon adding certain objects.

--- HedgeLib/Sets/SetObject.cs
+++ HedgeLib/Sets/SetObject.cs
@@ -26,6 +26,21 @@ namespace HedgeLib.Sets
 
 			foreach (var param in type.Parameters)
 			{
+                if (param is SetObjectTypeParamGroup group)
+                {
+                    var g = new SetObjectParamGroup(group.Padding);
+                    var groupParams = g.Parameters;
+
+                    foreach (var p in group.Parameters)
+                    {
+                        groupParams.Add(new SetObjectParam(p.DataType,
+					        p.DefaultValue));
+                    }
+
+                    Parameters.Add(g);
+                    continue;
+                }
+
 				Parameters.Add(new SetObjectParam(param.DataType,
 					param.DefaultValue));
 			}
@@ -37,6 +52,12 @@ namespace HedgeLib.Sets
 			return (CustomData.ContainsKey(name)) ?
 				(T)CustomData[name].Data : default(T);
 		}
+
+        public T GetCustomDataValue<T>(string name, T defaultValue)
+        {
+            return (CustomData.ContainsKey(name)) ?
+                (T)CustomData[name].Data : defaultValue;
+        }
     }
 
     [Serializable]
