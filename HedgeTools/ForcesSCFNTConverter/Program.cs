72e9378281399402d332d95e9734cd9a97b0468e
KTE2399 <sajidur78@gmail.com>
2018-05-21 04:36:29 +0600

    Add SCFNT -> OTF/TTF Support to ForcesSCFNTConverter
    have fun ripping fonts idunno what you do with this kind of stuff

--- HedgeTools/ForcesSCFNTConverter/Program.cs
+++ HedgeTools/ForcesSCFNTConverter/Program.cs
@@ -2,6 +2,8 @@
 using HedgeLib.Headers;
 using System.IO;
 using System;
+using HedgeLib.Exceptions;
+using System.Text;
 
 namespace ForcesSCFNTConverter
 {
@@ -9,7 +11,7 @@ namespace ForcesSCFNTConverter
     {
         // Variables/Constants
         public static BINAHeader Header = new BINAHeader() { Version = 210 };
-        
+
         // Methods
         public static void Main(string[] args)
         {
@@ -21,13 +23,14 @@ namespace ForcesSCFNTConverter
             }
 
             // Checks if the input has .otf or .ttf
-            if (!CheckExtension(args[0], ".otf", ".ttf"))
+            if (!CheckExtension(args[0], ".otf", ".ttf", ".scfnt"))
             {
                 Console.WriteLine("Error: Input file doesn't have a .otf or .ttf extension!\n");
                 ShowHelp();
                 return;
             }
 
+            bool isScfnt = args[0].ToLower().EndsWith(".scfnt");
             // Input
             FileInfo fileInfo = new FileInfo(args[0]);
             // Output
@@ -37,20 +40,60 @@ namespace ForcesSCFNTConverter
             if (args.Length > 1)
                 outputFilePath = args[1];
 
-            // Reads font into Memory
-            var data = File.ReadAllBytes(fileInfo.FullName);
-            // Grabs the file name
-            var name = Path.GetFileNameWithoutExtension(fileInfo.Name);
             // Opens a new File
-            using (var stream = File.OpenWrite(outputFilePath))
+            using (var stream = !isScfnt ? File.OpenWrite(outputFilePath)
+                : File.OpenRead(args[0]))
             {
-                BINAWriter writer = new BINAWriter(stream, BINA.BINATypes.Version2, false, true);
-                writer.WriteSignature("KFCS1000");      // "KFC" signature
-                writer.AddString("FontName", name, 8);  // Pointer to file name
-                writer.WriteNulls(0x10);                // Unknown
-                writer.Write((long)data.Length);        // Font size
-                writer.Write(data);                     // Font data
-                writer.FinishWrite(Header);             // Finalise writing
+                if (isScfnt)
+                {
+                    ExportFont(stream, outputFilePath);
+                }
+                else
+                {
+                    // Reads font into Memory
+                    var data = File.ReadAllBytes(fileInfo.FullName);
+                    // Grabs the file name
+                    var name = Path.GetFileNameWithoutExtension(fileInfo.Name);
+                    ExportScfnt(stream, name, data);
+                }
+            }
+        }
+
+        public static void ExportScfnt(Stream stream, string name, byte[] data)
+        {
+            BINAWriter writer = new BINAWriter(stream, BINA.BINATypes.Version2, false, true);
+            writer.WriteSignature("KFCS1000");      // "KFC" signature
+            writer.AddString("FontName", name, 8);  // Pointer to file name
+            writer.WriteNulls(0x10);                // Unknown
+            writer.Write((long)data.Length);        // Font size
+            writer.Write(data);                     // Font data
+            writer.FinishWrite(Header);             // Finalise writing
+        }
+
+        public static void ExportFont(Stream stream, string outputFile)
+        {
+            var reader = new BINAReader(stream, BINA.BINATypes.Version2);
+            reader.ReadHeader();
+
+            var sig = reader.ReadSignature(8);
+
+            if (sig != "KFCS1000")
+                throw new InvalidSignatureException("KFCS1000", sig);
+
+            var fontName = reader.GetString();
+            reader.JumpAhead(4);
+            reader.JumpAhead(0x10);
+            var length = reader.ReadInt64();
+            var fntSig = reader.ReadSignature();
+
+            outputFile = fntSig == "OTTO" ? Path.ChangeExtension(outputFile, ".otf")
+                : Path.ChangeExtension(outputFile, ".ttf");
+            var data = reader.ReadBytes((int)length - 4);
+
+            using (var fntStream = File.Create(outputFile))
+            {
+                fntStream.Write(Encoding.Default.GetBytes(fntSig), 0, 4);
+                fntStream.Write(data, 0, data.Length);
             }
         }
 
@@ -65,12 +108,12 @@ namespace ForcesSCFNTConverter
         public static void ShowHelp()
         {
             Console.WriteLine("ForcesSCFNTConverter input [output]");
-            Console.WriteLine("By: Slashiee and SuperSonic16");
+            Console.WriteLine("By: Slashiee, SuperSonic16 and Sajid");
 
             Console.WriteLine();
             Console.WriteLine("Arguments (arguments surrounded by square brackets are optional):");
-            Console.WriteLine("- input: \tPath to an OTF or TTF file.");
-            Console.WriteLine("- [output]: \tPath to save the SCFNT file");
+            Console.WriteLine("- input: \tPath to an OTF, TTF or SCFNT file.");
+            Console.WriteLine("- [output]: \tPath to save the SCFNT or TTF/OTF file");
 
             Pause();
         }
