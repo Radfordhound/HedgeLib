553f8a2808fa6034ffd05ef65e8069d9b071c369
Michael <goldtextwitch@outlook.com>
2018-05-19 23:31:13 -0700

    Add ForcesSCFNTGenerator
    This tool allows you to convert any TTF or OTF into a scfnt compatible with Sonic Forces.
    
    Co-Authored-By: thesupersonic16 <supersonic16bit@gmail.com>

---
+++ HedgeTools/ForcesSCFNTGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using HedgeLib.IO;
+using HedgeLib.Headers;
+using System.IO;
+
+namespace ForcesSCFNTGenerator
+{
+    class Program
+    {
+        public static BINAHeader Header = new BINAHeader() { Version = 210 };
+
+        static void Main(string[] args)
+        {
+            FileInfo fileInfo = new FileInfo(args[0]);
+            var data = File.ReadAllBytes(fileInfo.FullName);
+            using (var stream = File.OpenWrite(Path.ChangeExtension(fileInfo.FullName, "scfnt")))
+            {
+                BINAWriter writer = new BINAWriter(stream, BINA.BINATypes.Version2, false, true);
+                writer.WriteSignature("KFCS1000");
+                writer.Write((long)data.Length);
+                writer.WriteNulls(0x10);
+                writer.Write((long)data.Length);
+                writer.Write(data);
+                writer.FinishWrite(Header);
+            }
+        }
+    }
+}
