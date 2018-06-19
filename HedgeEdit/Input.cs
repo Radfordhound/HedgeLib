6b18f060c239b84c710c1765f087781b461c7741
Radfordhound <grahamlrscott@gmail.com>
2018-06-18 22:01:40 -0500

    Made my first mistake with the new version!!!! 🎉

---
+++ HedgeEdit/Input.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace HedgeEdit
+{
+    public static class Input
+    {
+        // Variables/Constants
+        public static Keys KeyState, PrevKeyState;
+    }
+}
