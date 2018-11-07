d393ae633357bfeefba9a83ba9bd9d453ba7df3c
Radfordhound <grahamlrscott@gmail.com>
2018-11-06 21:05:20 -0600

    Make ArchiveData implement IComparable Should allow you to sort lists of ArchiveData objects (and anything that extends from it, such as ArchiveFile) by name, alphabetically.

--- HedgeLib/Archives/ArchiveData.cs
+++ HedgeLib/Archives/ArchiveData.cs
@@ -2,7 +2,7 @@
 
 namespace HedgeLib.Archives
 {
-    public class ArchiveData
+    public class ArchiveData : IComparable
     {
         // Variables/Constants
         public string Name;
@@ -11,6 +11,18 @@ namespace HedgeLib.Archives
         public virtual void Extract(string filePath)
         {
             throw new NotImplementedException();
+        }
+
+        public virtual int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+
+            if (obj is ArchiveData data)
+                return Name.CompareTo(data.Name);
+
+            throw new NotImplementedException(
+                $"Cannot compare {GetType()} to {obj.GetType()}!");
         }
     }
 }
