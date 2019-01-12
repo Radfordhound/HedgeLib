a38b7d6b4340809c1f8d268793e229fca3af0347
Radfordhound <grahamlrscott@gmail.com>
2019-01-12 01:33:04 -0600

    Attempt to force vista style Probably have to include vista plugin under styles lol

--- HedgeEdit/src/main.cpp
+++ HedgeEdit/src/main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 #include "GFX\gfx.h"
 #include <QApplication>
+#include <QStyleFactory> // TODO
+#include <QMessageBox> // TODO
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	QApplication::setStyle(QStyleFactory::create("WindowsVista"));
+
+	QMessageBox Msgbox;
+	Msgbox.setText(QApplication::style()->metaObject()->className());
+	Msgbox.exec();
+
 	HedgeEdit::GFX::InitGFX();
 
 	HedgeEdit::UI::MainWindow w;
