934cc0973757ef53f75678d53a789fe90dc9052c
Radfordhound <grahamlrscott@gmail.com>
2019-01-22 03:19:13 -0600

    Use new way to get pointers from 32-bit offsets Instead of storing a relative offset that gets added to the address of the pointer itself (which created plenty of problems), use a static vector that pointers get added to/removed from and store an index to one of these pointers in the 32-bit "offsets". Not as efficient but not exactly inefficient either, and it seems to fix all the problems with the old method!

--- HedgeLib/include/IO/offsets.h
+++ HedgeLib/include/IO/offsets.h
@@ -20,42 +20,91 @@ namespace HedgeLib::IO
 		constexpr OffsetBase(const OffsetType offset) : o(offset) {}
 		constexpr OffsetBase(std::nullptr_t) : o(0) {}
 
+		inline void Set(const DataType* ptr) noexcept;
+		inline OffsetBase(const DataType* ptr) noexcept;
+
 		inline DataType* Get() const noexcept;
 		inline void EndianSwap();
 
 		constexpr void Fix(const std::uintptr_t origin,
 			const bool swapEndianness = false) noexcept;
 
-		inline void Set(const DataType* ptr);
-
 		template<typename CastedType>
 		inline CastedType* GetAs() const noexcept;
 	};
 
-#ifdef x86
-#define HEDGELIB_OFFSET32_TYPE std::uint32_t
-#elif x64
-#define HEDGELIB_OFFSET32_TYPE std::int32_t
+#ifdef x64
+	namespace detail
+	{
+		std::uint32_t Addx64Pointer(
+			const std::uintptr_t ptr) noexcept;
+		std::uintptr_t Getx64Pointer(
+			const std::uint32_t index) noexcept;
+		void Setx64Pointer(const std::uint32_t index,
+			const std::uintptr_t ptr) noexcept;
+		void Removex64Pointer(
+			const std::uint32_t index) noexcept;
+	}
 #endif
 
 	template<typename DataType>
-	struct OffsetBase<HEDGELIB_OFFSET32_TYPE, DataType>
+	struct OffsetBase<std::uint32_t, DataType>
 	{
 	protected:
-		HEDGELIB_OFFSET32_TYPE o = 0;
+		std::uint32_t o = 0;
 
 	public:
 		constexpr OffsetBase() = default;
-		constexpr OffsetBase(const HEDGELIB_OFFSET32_TYPE offset) : o(offset) {}
 		constexpr OffsetBase(std::nullptr_t) : o(0) {}
+
+		inline void Set(const DataType* ptr) noexcept
+		{
+#ifdef x86
+			o = static_cast<std::uint32_t>(
+				reinterpret_cast<std::uintptr_t>(ptr));
+#elif x64
+			if (o == 0)
+			{
+				o = detail::Addx64Pointer(reinterpret_cast
+					<std::uintptr_t>(ptr));
+			}
+			else
+			{
+				detail::Setx64Pointer(o, reinterpret_cast
+					<std::uintptr_t>(ptr));
+			}
+#endif
+		}
+
+		inline OffsetBase(const DataType* ptr) noexcept
+		{
+#ifdef x86
+			Set(ptr);
+#elif x64
+			o = detail::Addx64Pointer(reinterpret_cast
+				<std::uintptr_t>(ptr));
+#endif
+		}
+
+#ifdef x64
+		inline ~OffsetBase() noexcept
+		{
+			detail::Removex64Pointer(o);
+		}
+
+		void operator= (const OffsetBase& v) noexcept
+		{
+			o = detail::Addx64Pointer(detail::Getx64Pointer(v.o));
+		}
+#endif
 
 		inline DataType* Get() const noexcept
 		{
 #ifdef x86
 			return reinterpret_cast<DataType*>(o);
 #elif x64
-			return reinterpret_cast<DataType*>(reinterpret_cast
-				<const std::uintptr_t>(this) + o);
+			return reinterpret_cast<DataType*>(
+				detail::Getx64Pointer(o));
 #endif
 		}
 
@@ -69,27 +118,13 @@ namespace HedgeLib::IO
 			const bool swapEndianness = false) noexcept
 		{
 			if (swapEndianness)
-				HedgeLib::IO::Endian::SwapRecursive(this->o);
+				HedgeLib::IO::Endian::SwapRecursive(o);
 
 #ifdef x86
 			o += origin;
 #elif x64
-			o = static_cast<HEDGELIB_OFFSET32_TYPE>(((
-				origin + static_cast<std::uint32_t>(o)) -
-				reinterpret_cast<std::uintptr_t>(this)));
-#endif
-		}
-
-		inline void Set(const DataType* ptr)
-		{
-#ifdef x86
-			o = static_cast<HEDGELIB_OFFSET32_TYPE>(
-				reinterpret_cast<std::uintptr_t>(ptr));
-#elif x64
-			std::intptr_t t = reinterpret_cast<std::intptr_t>(this);
-			std::intptr_t p = reinterpret_cast<std::intptr_t>(ptr);
-
-			o = static_cast<HEDGELIB_OFFSET32_TYPE>(p - t);
+			o = detail::Addx64Pointer(origin +
+				static_cast<std::uintptr_t>(o));
 #endif
 		}
 
@@ -108,8 +143,18 @@ namespace HedgeLib::IO
 
 	public:
 		constexpr OffsetBase() = default;
-		constexpr OffsetBase(const std::uint64_t offset) : o(offset) {}
 		constexpr OffsetBase(std::nullptr_t) : o(0) {}
+
+		inline void Set(const DataType* ptr) noexcept
+		{
+			o = static_cast<std::uint64_t>(
+				reinterpret_cast<std::uintptr_t>(ptr));
+		}
+
+		inline OffsetBase(const DataType* ptr) noexcept
+		{
+			Set(ptr);
+		}
 
 		inline DataType* Get() const noexcept
 		{
@@ -129,12 +174,6 @@ namespace HedgeLib::IO
 				HedgeLib::IO::Endian::SwapRecursive(this->o);
 
 			o += origin;
-		}
-
-		inline void Set(const DataType* ptr)
-		{
-			o = static_cast<std::uint64_t>(
-				reinterpret_cast<std::uintptr_t>(ptr));
 		}
 
 		template<typename CastedType>
@@ -159,10 +198,10 @@ namespace HedgeLib::IO
 	struct DataOffset : public OffsetBase<OffsetType, DataType>
 	{
 		constexpr DataOffset() = default;
-		constexpr DataOffset(const OffsetType offset) :
-			OffsetBase<OffsetType, DataType>(offset) {}
 		constexpr DataOffset(std::nullptr_t) :
 			OffsetBase<OffsetType, DataType>(nullptr) {}
+		constexpr DataOffset(const DataType* ptr) :
+			OffsetBase<OffsetType, DataType>(ptr) {}
 
 		inline operator DataType*() const noexcept
 		{
@@ -182,11 +221,6 @@ namespace HedgeLib::IO
 		inline DataType& operator[] (const int index) const noexcept
 		{
 			return (this->Get())[index];
-		}
-
-		constexpr OffsetType& GetInternalOffset() noexcept
-		{
-			return this->o;
 		}
 
 		inline void FixOffset(const HedgeLib::IO::File& file,
@@ -234,7 +268,7 @@ namespace HedgeLib::IO
 	};
 
 	template<typename DataType>
-	using DataOffset32 = DataOffset<HEDGELIB_OFFSET32_TYPE, DataType>;
+	using DataOffset32 = DataOffset<std::uint32_t, DataType>;
 
 	template<typename DataType>
 	using DataOffset64 = DataOffset<std::uint64_t, DataType>;
@@ -341,7 +375,7 @@ namespace HedgeLib::IO
 	};
 
 	template<typename DataType, typename CountType = std::uint32_t>
-	using ArrOffset32 = ArrOffset<HEDGELIB_OFFSET32_TYPE, DataType, CountType>;
+	using ArrOffset32 = ArrOffset<std::uint32_t, DataType, CountType>;
 
 	template<typename DataType, typename CountType = std::uint64_t>
 	using ArrOffset64 = ArrOffset<std::uint64_t, DataType, CountType>;
---
+++ HedgeLib/src/IO/offsets.cpp
@@ -0,0 +1,75 @@
+#include "IO/offsets.h"
+#include "IO/endian.h"
+#include <optional>
+#include <vector>
+#include <cstdint>
+#include <cstddef>
+
+#ifdef x64
+namespace HedgeLib::IO::detail
+{
+	static std::vector<std::optional<std::uintptr_t>>
+		dataOffset32Ptrs = { std::nullopt };
+
+	static std::size_t nextFreeIndex = 1;
+
+	std::uint32_t Addx64Pointer(const std::uintptr_t ptr) noexcept
+	{
+		if (nextFreeIndex == dataOffset32Ptrs.size())
+		{
+			// No "free" (unused) spots available; Add pointer
+			dataOffset32Ptrs.push_back(ptr);
+			++nextFreeIndex;
+			return static_cast<std::uint32_t>(
+				dataOffset32Ptrs.size() - 1);
+		}
+		else
+		{
+			// Set pointer
+			std::uint32_t i = static_cast<std::uint32_t>(nextFreeIndex);
+			dataOffset32Ptrs[nextFreeIndex] = ptr;
+
+			// Determine next free index
+			while (++nextFreeIndex < dataOffset32Ptrs.size())
+			{
+				if (!dataOffset32Ptrs[nextFreeIndex].has_value())
+					break;
+			}
+
+			return i;
+		}
+	}
+
+	std::uintptr_t Getx64Pointer(const std::uint32_t index) noexcept
+	{
+		return dataOffset32Ptrs[index].value();
+	}
+
+	void Setx64Pointer(const std::uint32_t index,
+		const std::uintptr_t ptr) noexcept
+	{
+		dataOffset32Ptrs[index] = ptr;
+	}
+
+	void Removex64Pointer(const std::uint32_t index) noexcept
+	{
+		if (index == 0)
+			return;
+
+		if (index == (dataOffset32Ptrs.size() - 1) &&
+			nextFreeIndex == dataOffset32Ptrs.size())
+		{
+			dataOffset32Ptrs.pop_back();
+			--nextFreeIndex;
+		}
+		else
+		{
+			dataOffset32Ptrs[index] = std::nullopt;
+			if (index < nextFreeIndex)
+			{
+				nextFreeIndex = index;
+			}
+		}
+	}
+}
+#endif
