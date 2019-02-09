e4d4fb1ddd4ba440a0482c3cc5ffa16ea921bd0b
Radfordhound <grahamlrscott@gmail.com>
2019-02-08 19:16:51 -0600

    frick

---
+++ HedgeLib/include/Archives/pac.h
@@ -0,0 +1,9 @@
+#ifndef HPAC_H_INCLUDED
+#define HPAC_H_INCLUDED
+#include "IO/dataSignature.h"
+
+namespace HedgeLib::Archives
+{
+	static constexpr HedgeLib::IO::DataSignature PACxSignature = "PACx";
+}
+#endif
