76b8d8112fffa677349b21a24ab4c7ba71f40626
Radfordhound <grahamlrscott@gmail.com>
2019-05-24 15:23:47 -0500

    not forgetting files would be nice

---
+++ options.lua
@@ -0,0 +1,15 @@
+-- Library Type Option
+newoption(
+{
+	trigger = "type",
+	value = "shared/static",
+	description = "What type of library HedgeLib is/will be.",
+	default = "static",
+	allowed =
+	{
+		{ "static", "Static library (e.g. .lib/.a)" },
+        { "shared", "Shared library (e.g. .dll/.so)" },
+	}
+})
+
+LibType = _OPTIONS["type"]
