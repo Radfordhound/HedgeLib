0cdf2d6e9b2625eb39582986a5c0a2ddc6b7c11a
Radfordhound <grahamlrscott@gmail.com>
2019-05-24 14:53:20 -0500

    Make managed objects add/remove 32-bit pointers

--- HedgeLib/include/HedgeLib/Managed/Objects.h
+++ HedgeLib/include/HedgeLib/Managed/Objects.h
@@ -1,9 +1,24 @@
 #pragma once
 #include "Pointers.h"
+#include "Helpers.h"
 #include <variant>
 
 namespace HedgeLib
 {
+#ifdef x64
+    template<typename T>
+    using x64AddAbsPtrs32_t = decltype(std::declval<T&>().x64AddAbsPtrs32());
+
+    template<typename T>
+    constexpr bool Hasx64AddAbsPtrs32Function = is_detected_v<x64AddAbsPtrs32_t, T>;
+
+    template<typename T>
+    using x64RemoveAbsPtrs32_t = decltype(std::declval<T&>().x64RemoveAbsPtrs32());
+
+    template<typename T>
+    constexpr bool Hasx64RemoveAbsPtrs32Function = is_detected_v<x64RemoveAbsPtrs32_t, T>;
+#endif
+
     template<typename T, typename P,
         template<typename> typename PGetter>
     class ParentObject
@@ -17,7 +32,17 @@ namespace HedgeLib
         }
 
     public:
-        inline ParentObject() : ptr(std::unique_ptr<T>(new T())) {}
+        inline ParentObject() : ptr(std::unique_ptr<T>(new T()))
+        {
+#ifdef x64
+            // Add 32-bit absolute pointers if necessary
+            if constexpr (Hasx64AddAbsPtrs32Function<T>)
+            {
+                std::get<std::unique_ptr<T>>(ptr)->x64AddAbsPtrs32();
+            }
+#endif
+        }
+
         inline ParentObject(T* ptr) : ptr(std::unique_ptr<T>(ptr)) {}
 
         inline T* Get() const noexcept
@@ -26,6 +51,21 @@ namespace HedgeLib
                 PGetter<T>()(std::get<P>(ptr).get()) :    // Get custom pointer
                 std::get<std::unique_ptr<T>>(ptr).get();  // Get normal pointer
         }
+
+#ifdef x64
+        inline ~ParentObject()
+        {
+            // Remove 32-bit absolute pointers if necessary
+            if constexpr (Hasx64RemoveAbsPtrs32Function<T>)
+            {
+                if (!IsCustomPtr())
+                {
+                    std::get<std::unique_ptr<T>>(
+                        ptr)->x64RemoveAbsPtrs32();
+                }
+            }
+        }
+#endif
 
         inline void EndianSwap()
         {
