d92bd879613e8d790393fc3a6ad318f89391712c
Radfordhound <grahamlrscott@gmail.com>
2019-06-02 19:28:37 -0500

    Merge managed and direct Vector types Didn't really make sense to have the managed type extend from the direct type in this case when the managed type doesn't make it non-POD.

--- HedgeLib/include/HedgeLib/Direct/Math/Vector.h
+++ HedgeLib/include/HedgeLib/Direct/Math/Vector.h
@@ -8,6 +8,12 @@ struct hl_Vector2
     float Y;
 
 #ifdef __cplusplus
+    constexpr hl_Vector2() : X(0), Y(0) {}
+    constexpr hl_Vector2(float x, float y) :
+        X(x), Y(y) {}
+
+    // TODO: Add additonal operators and such
+
     HL_INLN_ENDIAN_SWAP_CPP()
     {
         hl_SwapFloat(&X);
@@ -28,6 +34,15 @@ struct hl_Vector3
     float Z;
 
 #ifdef __cplusplus
+    constexpr hl_Vector3() : X(0), Y(0), Z(0) {}
+    constexpr hl_Vector3(float x, float y,
+        float z) : X(x), Y(y), Z(z) {}
+
+    constexpr hl_Vector3(const hl_Vector2& v) :
+        hl_Vector3(v.X, v.Y, 0.0f) {}
+
+    // TODO: Add additonal operators and such
+
     HL_INLN_ENDIAN_SWAP_CPP()
     {
         hl_SwapFloat(&X);
@@ -50,6 +65,18 @@ struct hl_Vector4
     float W;
 
 #ifdef __cplusplus
+    constexpr hl_Vector4() : X(0), Y(0), Z(0), W(0) {}
+    constexpr hl_Vector4(float x, float y,
+        float z, float w) : X(x), Y(y), Z(z), W(w) {}
+
+    constexpr hl_Vector4(const hl_Vector2& v) :
+        hl_Vector4(v.X, v.Y, 0.0f, 0.0f) {}
+
+    constexpr hl_Vector4(const hl_Vector3& v) :
+        hl_Vector4(v.X, v.Y, v.Z, 0.0f) {}
+
+    // TODO: Add additonal operators and such
+
     HL_INLN_ENDIAN_SWAP_CPP()
     {
         hl_SwapFloat(&X);
