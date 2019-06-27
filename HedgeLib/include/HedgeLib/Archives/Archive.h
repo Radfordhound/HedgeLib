95a1f5d613230f8e3d2cae6a8601a829f9c3c348
Radfordhound <grahamlrscott@gmail.com>
2019-06-27 01:10:02 -0500

    Add hl_ArchiveFileEntry and functions to create them

--- HedgeLib/include/HedgeLib/Archives/Archive.h
+++ HedgeLib/include/HedgeLib/Archives/Archive.h
@@ -1,13 +1,42 @@
 #pragma once
 #include "../HedgeLib.h"
+#include "../Array.h"
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct hl_Blob;
+struct hl_ArchiveFileEntry
+{
+    size_t Size;        // The size of the data, or 0 if Data points to a file name.
+    const char* Name;   // The name of the file + its extension. Does NOT include the path!!
+    const void* Data;   // Points to the file path if Size == 0, or the file's data if Size != 0.
+};
+
 // TODO: Should this return an HL_RESULT?
 HL_API void hl_ExtractArchive(const struct hl_Blob* blob, const char* dir);
+HL_API void hl_CreateArchiveFileEntry(const char* filePath,
+    struct hl_ArchiveFileEntry* entry);
+
+inline void hl_CreateArchiveFileEntryData(size_t dataSize,
+    const char* name, const void* data, struct hl_ArchiveFileEntry* entry)
+{
+    *entry =
+    {
+        dataSize, name, data
+    };
+}
+
+HL_API struct hl_ArchiveFileEntry* hl_CreateArchiveFileEntries(
+    const char** files, size_t fileCount);
+
+inline struct hl_ArchiveFileEntry* hl_CreateArchiveFileEntriesArr(
+    struct hl_PtrArray* arr)
+{
+    return hl_CreateArchiveFileEntries(
+        (const char**)arr->Data, arr->Count);
+}
 
 #ifdef __cplusplus
 }
--- HedgeLib/src/Archives/Archive.cpp
+++ HedgeLib/src/Archives/Archive.cpp
@@ -1,6 +1,8 @@
 #include "HedgeLib/Archives/Archive.h"
 #include "HedgeLib/Archives/PACx.h"
 #include "../INBlob.h"
+#include <cstring>
+#include <cstdlib>
 
 void hl_ExtractArchive(const struct hl_Blob* blob, const char* dir)
 {
@@ -20,4 +22,47 @@ void hl_ExtractArchive(const struct hl_Blob* blob, const char* dir)
         // TODO: Should we return an error or something??
         break;
     }
+}
+
+void hl_CreateArchiveFileEntry(const char* filePath,
+    struct hl_ArchiveFileEntry* entry)
+{
+    // Get the file name (without the directory)
+    size_t filePathLen = std::strlen(filePath);
+    const char* name = nullptr;
+
+    if (filePathLen > 0)
+    {
+        for (size_t i = (filePathLen - 1); i >= 0; --i)
+        {
+            if (filePath[i] == '/' || filePath[i] == '\\')
+            {
+                name = &filePath[i + 1];
+                break;
+            }
+        }
+    }
+
+    // Create the file entry
+    *entry =
+    {
+        //HL_ARCHIVE_FILE_ENTRY_TYPE_FILEPATH,
+        0, (name) ? name : filePath,
+        filePath
+    };
+}
+
+struct hl_ArchiveFileEntry* hl_CreateArchiveFileEntries(
+    const char** files, size_t fileCount)
+{
+    // Create file entries
+    hl_ArchiveFileEntry* fileEntries = static_cast<hl_ArchiveFileEntry*>(
+        std::malloc(sizeof(hl_ArchiveFileEntry) * fileCount));
+
+    for (size_t i = 0; i < fileCount; ++i)
+    {
+        hl_CreateArchiveFileEntry(files[i], &(fileEntries[i]));
+    }
+
+    return fileEntries;
 }
