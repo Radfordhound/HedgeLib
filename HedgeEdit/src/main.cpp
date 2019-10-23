34022e3d5daa2eefc36417f0e33ab4e323ac9c26
Radfordhound <Grahamlrscott@gmail.com>
2019-10-23 00:23:45 -0500

    Test to see if AppVeyor styles work when not explicitly set

--- HedgeEdit/src/main.cpp
+++ HedgeEdit/src/main.cpp
@@ -1,12 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QStyleFactory> // TODO
 
 int main(int argc, char *argv[])
 {
     using namespace HedgeEdit;
     QApplication a(argc, argv);
-    QApplication::setStyle(QStyleFactory::create("WindowsVista"));
 
     UI::MainWindow w;
     w.show();
