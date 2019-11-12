b9bf131a58d8127a0c17c6bbf92284487b8d684a
Radfordhound <Grahamlrscott@gmail.com>
2019-11-11 21:37:17 -0600

    C++-ify HedgeOffsets

--- HedgeTools/HedgeOffsets/src/main.cpp
+++ HedgeTools/HedgeOffsets/src/main.cpp
@@ -1,41 +1,29 @@
-#include "HedgeLib/Archives/PACx.h"
 #include "HedgeLib/IO/BINA.h"
 #include "HedgeLib/IO/File.h"
 #include "HedgeLib/Blob.h"
 #include <iostream>
 
-HL_RESULT PrintOffsets(const char* filePath)
+void PrintOffsets(const char* filePath)
 {
     // Open file
-    hl_File file;
-    HL_RESULT result = file.OpenRead(filePath);
-    if (HL_FAILED(result)) return result;
+    hl::File file = hl::File(filePath);
 
     // Get signature
-    uint32_t sig;
-    result = file.ReadNoSwap(sig);
-    if (HL_FAILED(result)) return result;
+    std::uint32_t sig;
+    file.ReadNoSwap(sig);
 
     // Go back to beginning of file
-    result = file.JumpBehind(4);
-    if (HL_FAILED(result)) return result;
+    file.JumpBehind(4);
 
     // BINA V2
-    hl_BlobPtr blob;
     if (sig == HL_BINA_SIGNATURE)
     {
         // Read file
-        hl_Blob* blobPtr;
-        result = hl_BINAReadV2(&file, &blobPtr);
-        if (HL_FAILED(result)) return result;
-
-        blob = blobPtr;
+        hl::Blob blob = DBINAReadV2(file);
 
         // Get header
-        const hl_BINAV2Header* header = static_cast<hl_BINAV2Header*>(
-            hl_BlobGetRawData(blob));
-
-        uintptr_t headerPtr = reinterpret_cast<uintptr_t>(header);
+        const hl::BINAV2Header* header = blob.RawData<hl::BINAV2Header>();
+        std::uintptr_t headerPtr = reinterpret_cast<std::uintptr_t>(header);
 
         // Print header metadata
         std::cout << "Format: BINA v" << header->Version[0] << '.' <<
@@ -53,21 +41,21 @@ HL_RESULT PrintOffsets(const char* filePath)
         }
 
         // Get offset table pointer
-        const hl_BINAV2DataNode* dataNode = hl_BINAGetDataNodeV2(blob);
+        const hl::BINAV2DataNode* dataNode = hl::DBINAGetDataNodeV2(blob);
         if (!dataNode)
         {
             std::cout << "No data node present.\n";
-            return HL_SUCCESS;
+            return;
         }
 
-        const uint8_t* offTable = ((reinterpret_cast<const uint8_t*>(
+        const std::uint8_t* offTable = ((reinterpret_cast<const std::uint8_t*>(
             dataNode) + dataNode->Header.Size) - dataNode->OffsetTableSize);
 
         // Get string table pointer
-        uintptr_t data = (reinterpret_cast<uintptr_t>(dataNode + 1) +
+        std::uintptr_t data = (reinterpret_cast<std::uintptr_t>(dataNode + 1) +
             dataNode->RelativeDataOffset);
 
-        uintptr_t strTable = (data + dataNode->StringTable);
+        std::uintptr_t strTable = (data + dataNode->StringTable);
 
         // Print data node metadata
         std::cout << "Data Node Size: 0x" << std::hex <<
@@ -80,14 +68,16 @@ HL_RESULT PrintOffsets(const char* filePath)
             dataNode->OffsetTableSize << "\n\n";
 
         // Print offsets and relevant data
-        size_t offCount = 0;
-        const uint32_t* currentOffset = reinterpret_cast<const uint32_t*>(data);
-        const uint8_t* eof = (offTable + dataNode->OffsetTableSize);
+        std::size_t offCount = 0;
+        const std::uint32_t* currentOffset = reinterpret_cast<
+            const std::uint32_t*>(data);
+
+        const std::uint8_t* eof = (offTable + dataNode->OffsetTableSize);
 
         while (offTable < eof)
         {
             // Get next offset
-            if (!hl_BINANextOffset(&offTable, &currentOffset))
+            if (!hl::BINANextOffset(offTable, currentOffset))
             {
                 // Break if we've reached the last offset
                 break;
@@ -95,20 +85,20 @@ HL_RESULT PrintOffsets(const char* filePath)
 
             // Print info about this offset
             std::cout << "Offset #" << std::dec << ++offCount << ":\t{ pos: 0x" << std::hex <<
-                (reinterpret_cast<uintptr_t>(currentOffset) - headerPtr);
+                (reinterpret_cast<std::uintptr_t>(currentOffset) - headerPtr);
             
-            uintptr_t off;
+            std::uintptr_t off;
             if (header->Version[1] == '1')
             {
                 // 64-bit offsets
-                off = reinterpret_cast<uintptr_t>(HL_GETPTR64(const uint8_t,
-                    *reinterpret_cast<const uint64_t*>(currentOffset)));
+                off = reinterpret_cast<std::uintptr_t>(reinterpret_cast<
+                    const hl::DataOffset64<std::uint8_t>*>(currentOffset)->Get());
             }
             else
             {
                 // 32-bit offsets
-                off = reinterpret_cast<uintptr_t>(HL_GETPTR32(
-                    const uint8_t, *currentOffset));
+                off = reinterpret_cast<std::uintptr_t>(reinterpret_cast<
+                    const hl::DataOffset32<std::uint8_t>*>(currentOffset)->Get());
             }
 
             std::cout << ", val: 0x" << std::hex << (off - headerPtr) << " }";
@@ -121,8 +111,6 @@ HL_RESULT PrintOffsets(const char* filePath)
 
             std::cout << '\n';
         }
-
-        return HL_SUCCESS;
     }
 
     // TODO: BINA V1 Support
@@ -130,7 +118,21 @@ HL_RESULT PrintOffsets(const char* filePath)
     // TODO: Hedgehog Engine Support
     // TODO: Mirage Header Support
 
-    return HL_ERROR_UNSUPPORTED;
+    else
+    {
+        throw std::runtime_error("Unknown or unsupported format.");
+    }
+}
+
+void PrintUsage(std::ostream& s)
+{
+    s << "Usage: HedgeOffsets filePath [Options]\n";
+    s << "  -NP\tExit without prompting user to press enter.\n";
+    s << "  -?\tDisplays this help.\n\n";
+
+#ifdef _WIN32
+    s << "(Or just drag n' drop a file onto HedgeOffsets.exe)\n";
+#endif
 }
 
 int main(int argc, char* argv[])
@@ -140,28 +142,58 @@ int main(int argc, char* argv[])
 
     // Print usage if arguments are invalid
     int errorCode = EXIT_SUCCESS;
-    if (argc < 2)
+    const char* input = nullptr;
+    bool doPrompt = true, showedHelp = false;
+
+    try
     {
-        std::cout << "Usage: HedgeOffsets filePath\n";
-#ifdef _WIN32
-        std::cout << "(Or just drag n' drop a file onto HedgeOffsets.exe)\n";
-#endif
-    }
-    else
-    {
-        // Read file and print offsets
-        HL_RESULT result = PrintOffsets(argv[1]);
-        if (HL_FAILED(result))
+        // Loop through arguments
+        for (int i = 1; i < argc; ++i)
         {
-            std::cout << "ERROR: " << hl_GetResultString(result) << '\n';
-            errorCode = EXIT_FAILURE;
+            if (!std::strcmp(argv[i], "-NP"))
+            {
+                doPrompt = false;
+            }
+            else if (!showedHelp && !std::strcmp(argv[i], "-?"))
+            {
+                PrintUsage(std::cout);
+                showedHelp = true;
+            }
+            else if (!input)
+            {
+                input = argv[i];
+            }
+            else throw std::invalid_argument("Cannot handle multiple file paths.");
         }
+
+        // Act accordingly
+        if (input)
+        {
+            // Read file and print offsets
+            PrintOffsets(argv[1]);
+        }
+        else if (!showedHelp)
+        {
+            PrintUsage(std::cerr);
+        }
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr << "ERROR: " << ex.what() << '\n';
+        errorCode = EXIT_FAILURE;
     }
 
     // Pause the program until the user presses enter
-    std::cout << "Press enter to continue...\n";
-    std::cout.flush();
-    std::cin.get();
+    if (doPrompt)
+    {
+        std::cout << "Press enter to continue...\n";
+        std::cout.flush();
+        std::cin.get();
+    }
+    else
+    {
+        std::cout.flush();
+    }
 
     return errorCode;
 }
