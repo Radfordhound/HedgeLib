41cf26a3fde3ceb9b0d41b9ca41b0df26ec0ff52
Radfordhound <Grahamlrscott@gmail.com>
2020-10-07 19:33:41 -0500

    Also convert ERROR_FILE_NOT_FOUND to an HlResult

--- HedgeLib/src/hl_in_win32.c
+++ HedgeLib/src/hl_in_win32.c
@@ -8,6 +8,7 @@ HlResult hlINWin32GetResult(HRESULT hresult)
     case S_OK: return HL_RESULT_SUCCESS;
     case E_INVALIDARG: return HL_ERROR_INVALID_ARGS;
     case E_OUTOFMEMORY: return HL_ERROR_OUT_OF_MEMORY;
+    case __HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND): return HL_ERROR_NOT_FOUND;
 
     /* TODO: More errors. */
 
--- HedgeLib/src/hl_in_win32.h
+++ HedgeLib/src/hl_in_win32.h
@@ -6,10 +6,17 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 HlResult hlINWin32GetResult(HRESULT hresult);
 
 #define hlINWin32GetResultLastError() hlINWin32GetResult(\
     HRESULT_FROM_WIN32(GetLastError()))
 
+#ifdef __cplusplus
+}
+#endif
 #endif
 #endif
